1da43ab6a85164b57c89aff97189879c88dba991
Mostofa Kamal <emonlm10.me@gmail.com>
2019-11-23 10:15:29 +0600

    Modified admin folder

--- admin/upload2.php
+++ admin/upload2.php
@@ -5,15 +5,96 @@ $statusMsg = '';
 $category = $emp_name = '';
 
 // File upload path
-$targetDir = "../img/uploads/";
-$fileName = $_FILES["file"]["name"];
-$targetFilePath = $targetDir . $fileName;
-$fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+// $targetDir = "uploads/";
+// $fileName = basename($_FILES["file"]["name"]);
+// $targetFilePath = $targetDir . $fileName;
+// $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
 
-if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
+if(isset($_POST["submit"])){
+    // $newFileName = $_POST['file'];
+
+    // if(empty($_POST['file']))
+    // {
+    //     $newFileName = "temporary";
+    // }
+    // else
+    // {
+    //     $newFileName = strtolower(str_replace(' ','-',$newFileName));
+    // }
 	$name = $_POST['emp_name'];
 	$dept = $_POST['dept'];
-	$designation = $_POST['designation'];
+    $designation = $_POST['designation'];
+    
+    $file = $_FILES['file'];
+    $fileName = $file['name'];
+    $fileType = $file['type'];
+    $fileTempName = $file['temp_name'];
+    $fileError = $file['error'];
+    $fileSize = $file['size'];
+
+    $fileExt = explode('.',$fileName);
+    $fileActExt = strtolower(end($fileExt));
+
+    $allowed = array("jpg","jpeg","png","pdf");
+
+    if(in_array($fileActExt,$allowed))
+    {
+        if($fileError===0)
+        {
+            if($fileSize<2048)
+            {
+                $imageName = $fileName . "." . $fileActExt;
+                $fileDest = "img/uploads/" . $imageName;
+                if(empty($name)||empty($dept)||empty($designation))
+                {
+                    header('Location:'); 
+                    exit
+                }
+                else{
+                    $sql = "SELECT * FROM images";
+                    $stmt = mysqli_stmt_init($conn);
+                    if(!mysqli_stmt_prepare($stmt,$sql))
+                    {
+                        echo 'SQL statement failed!';
+                    }
+                    else{
+                        mysqli_stmt_execute($stmt);
+                        $result = mysqli_stmt_get_result($stmt);
+                        $rowCount = mysqli_num_rows($result);
+                        $setImageOrder = $rowCount + 1;
+
+
+                        $sql = "INSERT INTO images (file_name, uploaded_on,name,designation,department,category) VALUES (?,?,?,?,?,?)";
+                        $stmt = mysqli_stmt_init($conn);
+                        if(!mysqli_stmt_prepare($stmt,$sql))
+                        {
+                            echo 'SQL statement failed!';
+                        }
+                        else{
+                            mysqli_stmt_bind_param($stmt,"ssss",);
+                        }
+                    }
+                }
+            }
+            else{
+                echo 'File size is too big.';
+                exit();
+            }
+        }
+        else{
+            echo 'There is an error'
+        }
+    }
+    else{
+        echo 'File Type not supported. Please use jpg or png files';
+        exit();
+    }
+
+
+
+
+
+
     // Allow certain file formats
     $allowTypes = array('jpg','png','jpeg','gif','pdf');
     if(in_array($fileType, $allowTypes)){
@@ -63,8 +144,8 @@ $statusMsg = '';
 $category = '';
 
 // File upload path
-$targetDir = "../uploads/";
-$fileName = $_FILES["file"]["name"];
+$targetDir = "uploads/";
+$fileName = basename($_FILES["file"]["name"]);
 $targetFilePath = $targetDir . $fileName;
 $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
 
